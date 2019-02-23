7476deee87b7b3609d8519818e8d0ba95c807086
Nelson Gomez <ng2573@columbia.edu>
2019-02-23 04:21:11 -0500

    Remove dependency on BeautifulSoup4
    When downloading photo rosters from Canvas, a "slug_files" directory is
    created which contains each student's photos, neatly organized with the
    filename format "{UNI}.jpg", rendering the need for parsing a roster
    HTML unnecessary.

--- seatingchart.py
+++ seatingchart.py
@@ -10,7 +10,6 @@ import csv
 import os
 import random
 import sys
-from bs4 import BeautifulSoup
 
 
 def assert_file_exists(path):
@@ -67,12 +66,10 @@ assign_last_path  = working_dir_path("assign-last",  args.slug, "txt")
 ASSIGN_FIRST = assign_first_path if os.path.isfile(assign_first_path) else "/dev/null"
 ASSIGN_LAST  = assign_last_path  if os.path.isfile(assign_last_path)  else "/dev/null"
 
-# 3) a HTML page matching the courseworks print as single column roster feature, with student pictures
-roster_path = working_dir_path("roster", args.slug, "html")
-assert_file_exists(roster_path)
-ROSTER_PAGE = roster_path
+photos_relpath = args.slug + "_files"
+photos_container = os.path.join("out", args.slug, photos_relpath)
 
-# 4) a tsv file containing the format of the room
+# 3) a tsv file containing the format of the room
 layout_path = os.path.join("layouts", args.layout + ".txt")
 assert_file_exists(layout_path)
 LAYOUT = layout_path
@@ -168,16 +165,6 @@ for seat, uni in assignments.iteritems():
     output.writerow(list(uni)[:2] + [seat])
 
 
-#Load picture files
-soup = BeautifulSoup(open(ROSTER_PAGE), "html.parser")
-photos = {}
-for img in soup.findAll(class_="rosterImage"):
-    url = img["src"]
-    uni = img.findParent().findNextSibling().findNextSibling().string
-    photos[uni] = url
-
-
-
 #Write the chart
 room = [s for s in csv.reader(open(LAYOUT), delimiter="\t")]
 maxrow = max([len(x) for x in room])
@@ -219,11 +206,12 @@ with open(OUTPUT_CHART, "w") as html:
                 student = assignments[seat]
                 uni = student[2]
                 name = student[0]
-                try:
-                    photo = photos[uni]
+                photo_path = os.path.join(photos_container, uni + ".jpg")
+                photo_rel_path = os.path.join(photos_relpath, uni + ".jpg")
+                if os.path.isfile(photo_path):
                     html.write("""<span class="seat">%s</span><br> %s<br> <span class="name">%s</span><br> <img src="%s">"""
-                           % (seat, uni, name, photo))
-                except KeyError:
+                           % (seat, uni, name, photo_rel_path))
+                else:
                     html.write("""<span class="seat">%s</span><br> %s<br> <span class="name">%s</span><br> """
                            % (seat, uni, name))
             except KeyError:
