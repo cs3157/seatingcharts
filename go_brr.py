4509601bc4e091b6c3561ccc4ee9c22a3f2a3c99
Leslie Chang <zz2474@columbia.edu>
2022-11-03 17:37:43 -0400

    fixed bug in seatingchart, working on go_brr

--- go_brr.py
+++ go_brr.py
@@ -7,89 +7,95 @@ import os
 import random
 import sys
 import math
-
-OUT_PATH = "out"
-HTML_PATH = "~/html/seating"
-
-parser = argparse.ArgumentParser(
-    description="Go brrr with the seating charts")
-
-parser.add_argument("rooms",
-                    type=str,
-                    metavar='<rooms>')
-
-parser.add_argument("roster",
-                    type=str,
-                    metavar='<roster>')
+import seatingchart
 
 
-args = parser.parse_args()
-ROOMS_IN_ORDER = args.rooms
-STUDENT_LIST = args.roster
+def main():
+    students = [tuple(s) for s in csv.reader(open(STUDENT_LIST))]
+    random.shuffle(students)
+    student_count = len(students)
+    seat_count = 0
+    for img in glob.glob("images/*"):
+        if img.split('.')[1] == "jpeg":
+            os.rename(img, img.split('.')[0]+'.jpg')
 
-#students = [tuple(s) for s in csv.reader(open(STUDENT_LIST))][3:]
-students = [tuple(s) for s in csv.reader(open(STUDENT_LIST))]
-random.shuffle(students)
-student_count = len(students)
-seat_count = 0
-for img in glob.glob("images/*"):
-    if img.split('.')[1] == "jpeg":
-        os.rename(img, img.split('.')[0]+'.jpg')
+    os.system(f"rm -r {HTML_PATH}")
+    os.system(f"mkdir -p {HTML_PATH}")
+    os.system(f"touch {HTML_PATH}/seat.csv")
+    os.system(f"chmod 744 {HTML_PATH}/seat.csv")
+    os.system(f"chmod 755 {HTML_PATH}")
 
-os.system(f"rm -r {HTML_PATH}")
-os.system("mkdir -p %s" % HTML_PATH)
-os.system("touch %s/seat.csv" % HTML_PATH)
-os.system("chmod 744 %s/seat.csv" % HTML_PATH)
-os.system("echo %s >>  %s/seat.csv" % ("Uni, Name, Room, Seat", HTML_PATH))
-os.system("chmod 755 %s" % HTML_PATH)
+    with open(ROOMS_IN_ORDER, 'r') as f:
+        rooms = f.readlines()
+    rooms = [r for r in rooms if r != '\n']
+    rooms = list([z.strip().split() for z in rooms])
+    print(f"rooms:{rooms}")
+    for _, count in rooms:
+        seat_count += int(count)
+    if seat_count == student_count:
+        print(f"Total seats:{seat_count}")
+    elif seat_count < student_count:
+        print(
+            f"Total seats:{seat_count} < students:{student_count}, it won't work!")
+        return
+    elif seat_count > student_count:
+        print(
+            f"Total seats:{seat_count} > students:{student_count}, are you sure?")
 
-with open(ROOMS_IN_ORDER, 'r') as f:
-    rooms = f.readlines()
-rooms = [r for r in rooms if r != '\n']
-rooms = list([z.strip().split() for z in rooms])
-print(f"rooms:{rooms}")
-for _, count in rooms:
-    seat_count += int(count)
-print(f"Total seats:{seat_count}")
+    for room in rooms:
+        rname = room[0]
+        path = os.path.join(OUT_PATH, rname)
+        os.system("rm -rf %s" % path)
+        os.mkdir(path)
+        os.system(("ln -s %s %s" %
+                   ("images", path + "/" + "images")))
+        # print(("cp %s %s" %
+        #        ("images", path + "/" + "images")))
+        # os.system(("cp -rf %s %s" %
+        #            ("images", path + "/" + "images")))
+        csvfile = open(os.path.join(path, "roster_" + rname + ".csv"), "w")
+        output = csv.writer(csvfile)
+        # output.writerow("")
+        # output.writerow("")
+        # for i in range(math.ceil(student_count / seat_count * int(room[1]))):
+        for i in range(int(room[1])):
+            #    for i in range(int(room[1])):
+            if(len(students) > 0):
+                student = random.choice(students)
+                output.writerow(student)
+                students.remove(student)
+        csvfile.flush()
+        seatingchart.arrange_seat(rname, rname)
+        # os.system("./seatingchart.py %s %s" %
+        #           (rname, rname))
+        os.system("cp %s/%s/chart_%s.html %s/%s.html" %
+                  (OUT_PATH, rname, rname, HTML_PATH, rname))
+        os.system(
+            f"perl -F, -lane 's/$F[2]/$F[2], {rname}/; print' {OUT_PATH}/{rname}/list_{rname}.csv >> {HTML_PATH}/seat.csv")
+    #    os.system("cp %s/%s/list_%s.csv %s/%s.csv" %
+    #              (OUT_PATH, rname, rname, HTML_PATH, rname))
+        os.system("chmod 644 %s/%s.html" % (HTML_PATH, rname))
+    os.system("cp -r images %s/images" % HTML_PATH)
+    os.system("chmod 711 %s/images" % HTML_PATH)
+    os.system("chmod 644 %s/images/*.*" % HTML_PATH)
 
-for room in rooms:
-    rname = room[0]
-    path = os.path.join(OUT_PATH, rname)
-    os.system("rm -rf %s" % path)
-    os.mkdir(path)
-    os.system(("ln -s %s %s" %
-               ("images", path + "/" + "images")))
-    # print(("cp %s %s" %
-    #        ("images", path + "/" + "images")))
-    # os.system(("cp -rf %s %s" %
-    #            ("images", path + "/" + "images")))
-    csvfile = open(os.path.join(path, "roster_" + rname + ".csv"), "w")
-    output = csv.writer(csvfile)
-    output.writerow("")
-    output.writerow("")
-    for i in range(math.ceil(student_count / seat_count * int(room[1]))):
-        #    for i in range(int(room[1])):
-        if(len(students) > 0):
-            student = random.choice(students)
-            output.writerow(student)
-            students.remove(student)
-    csvfile.flush()
-    print(f"rname:{rname}")
-    os.system("./seatingchart.py %s %s" %
-              (rname, rname))
-    os.system("cp %s/%s/chart_%s.html %s/%s.html" %
-              (OUT_PATH, rname, rname, HTML_PATH, rname))
-    os.system(
-        f"perl -F, -lane 's/$F[2]/$F[2], {rname}/; print' {OUT_PATH}/{rname}/list_{rname}.csv >> {HTML_PATH}/seat.csv")
-#    os.system("cp %s/%s/list_%s.csv %s/%s.csv" %
-#              (OUT_PATH, rname, rname, HTML_PATH, rname))
-    os.system("chmod 644 %s/%s.html" % (HTML_PATH, rname))
-os.system("cp -r images %s/images" % HTML_PATH)
-os.system("chmod 711 %s/images" % HTML_PATH)
-os.system("chmod 644 %s/images/*.*" % HTML_PATH)
-# convert(f"/home/zz2474/html/seating/seat.csv",
-#         f"/home/zz2474/html/seating/seat.pdf")
-os.system("chmod 644 /home/zz2474/html/seating/seat.pdf")
-os.system("rm /home/zz2474/html/seating/seat.csv")
+    print("\033[01;92mSuccess!")
 
-print("\033[01;92mSuccess!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Go brrr with the seating charts")
+
+    parser.add_argument("rooms",
+                        type=str,
+                        metavar='<rooms>')
+
+    parser.add_argument("roster",
+                        type=str,
+                        metavar='<roster>')
+    args = parser.parse_args()
+    ROOMS_IN_ORDER = args.rooms
+    STUDENT_LIST = args.roster
+    OUT_PATH = "out"
+    HTML_PATH = "~/html/seating"
+    main()
--- seatingchart.py
+++ seatingchart.py
@@ -27,208 +27,211 @@ def working_dir_path(name, slug, extension):
     return os.path.join("out", slug, filename)
 
 
-parser = argparse.ArgumentParser(
-    description="Generate a seating chart based on the course roster")
+def arrange_seat(slug: str, layout: str, title: str = None, lefty: bool = False, debug: bool = False) -> None:
+    # 1) a list of seats, in order of preference, with optionally ignored blank lines and repeats allowed
+    #    basically tab/newline separated, with no difference between them
+    SEATS_IN_ORDER = os.path.join("layouts", layout + "_ordered.txt")
+    assert_file_exists(SEATS_IN_ORDER)
 
-parser.add_argument("slug",
-                    type=str,
-                    help="the \"out\" subdirectory to use as the working directory",
-                    metavar='<slug>')
+    # 1.5) a list of lefty seats, in order of preference, with optioonally ignored blank lines and
+    #      repeats allowed, basically tab/newline separted with no difference between them
+    if lefty:
+        LSEATS_IN_ORDER = os.path.join(
+            "layouts", layout + "_lefty_ordered.txt")
+        assert_file_exists(LSEATS_IN_ORDER)
 
-parser.add_argument("layout",
-                    type=str,
-                    help="the seating chart layout (classroom name) to use",
-                    metavar="<layout>")
+    # 2) a CSV list of students, one per line.
+    # ie what you get if you download a gradebook from courseworks
+    STUDENT_LIST = working_dir_path("roster", slug, "csv")
+    assert_file_exists(STUDENT_LIST)
 
-parser.add_argument("-t", "--title",
-                    default=None, type=str,
-                    help="human-readable name that will be written the top of seating chart",
-                    metavar="<title>")
+    # 2.5) a CSV list of students, one per line.
+    # ie what you get if you download a gradebook from courseworks
+    if lefty:
+        LSTUDENT_LIST = working_dir_path("lefty_roster", slug, "csv")
+        assert_file_exists(LSTUDENT_LIST)
 
-parser.add_argument("-l", "--lefty",
-                    action="store_true",
-                    help="assign seats to lefty students")
+    # two lists, of students to assign first and last
+    assign_first_path = working_dir_path("assign-first", slug, "txt")
+    assign_last_path = working_dir_path("assign-last",  slug, "txt")
 
-parser.add_argument("-d", "--debug",
-                    action="store_true",
-                    help="print debug messages")
+    ASSIGN_FIRST = assign_first_path if os.path.isfile(
+        assign_first_path) else "/dev/null"
+    ASSIGN_LAST = assign_last_path if os.path.isfile(
+        assign_last_path) else "/dev/null"
 
-args = parser.parse_args()
+    # photos_path = os.path.join("out", slug, slug + "_files")
+    photos_path = os.path.join(
+        "images")
 
+    # 3) a tsv file containing the format of the room
+    layout_path = os.path.join("layouts", layout + ".txt")
+    assert_file_exists(layout_path)
+    LAYOUT = layout_path
 
-# 1) a list of seats, in order of preference, with optionally ignored blank lines and repeats allowed
-#    basically tab/newline separated, with no difference between them
-SEATS_IN_ORDER = os.path.join("layouts", args.layout + "_ordered.txt")
-assert_file_exists(SEATS_IN_ORDER)
+    # outputs:
+    TITLE = title if title != None else f"{slug} Seating"
 
-# 1.5) a list of lefty seats, in order of preference, with optioonally ignored blank lines and
-#      repeats allowed, basically tab/newline separted with no difference between them
-if args.lefty:
-    LSEATS_IN_ORDER = os.path.join(
-        "layouts", args.layout + "_lefty_ordered.txt")
-    assert_file_exists(LSEATS_IN_ORDER)
+    # a CSV student id ordered list of assigned seats
+    OUTPUT_CSV = working_dir_path("list", slug, "csv")
 
-# 2) a CSV list of students, one per line.
-# ie what you get if you download a gradebook from courseworks
-STUDENT_LIST = working_dir_path("roster", args.slug, "csv")
-assert_file_exists(STUDENT_LIST)
+    # a pretty HTML version of uni <-> seats
+    OUTPUT_HTML = working_dir_path("list", slug, "html")
 
-# 2.5) a CSV list of students, one per line.
-# ie what you get if you download a gradebook from courseworks
-if args.lefty:
-    LSTUDENT_LIST = working_dir_path("lefty_roster", args.slug, "csv")
-    assert_file_exists(LSTUDENT_LIST)
+    # an html page with seat, student, and photo
+    OUTPUT_CHART = working_dir_path("chart", slug, "html")
 
-# two lists, of students to assign first and last
-assign_first_path = working_dir_path("assign-first", args.slug, "txt")
-assign_last_path = working_dir_path("assign-last",  args.slug, "txt")
+    # Now we're ready to assign seats
+    assignments = {}
 
-ASSIGN_FIRST = assign_first_path if os.path.isfile(
-    assign_first_path) else "/dev/null"
-ASSIGN_LAST = assign_last_path if os.path.isfile(
-    assign_last_path) else "/dev/null"
+    with open(SEATS_IN_ORDER, "r") as f:
+        seats = f.readlines()
+    seats = [s for s in seats if s[0] != '#']  # Strip comments
+    seats = list(itertools.chain.from_iterable(
+        [z.strip().split() for z in seats]))
 
-# photos_path = os.path.join("out", args.slug, args.slug + "_files")
-photos_path = os.path.join(
-    "images")
+    assign_last = [x.strip() for x in open(ASSIGN_LAST).readlines()]
+    assign_first = [x.strip() for x in open(ASSIGN_FIRST).readlines()]
 
-# 3) a tsv file containing the format of the room
-layout_path = os.path.join("layouts", args.layout + ".txt")
-assert_file_exists(layout_path)
-LAYOUT = layout_path
+    students = [tuple(s) for s in csv.reader(
+        open(STUDENT_LIST))][2:]  # Skip two header rows
+    random.shuffle(students)
 
-# outputs:
-TITLE = args.title if args.title != None else f"{args.slug} Seating"
+    # the assign_first/last students are shuffled randomly, so we need to pull them to the front/back
+    reassign = [x for x in students if x[0] in assign_last]
+    for x in reassign:
+        students.remove(x)
+    students.extend(reassign)
+    students.reverse()
 
-# a CSV student id ordered list of assigned seats
-OUTPUT_CSV = working_dir_path("list", args.slug, "csv")
+    reassign = [x for x in students if x[0] in assign_first]
+    for x in reassign:
+        students.remove(x)
+    students.extend(reassign)
 
-# a pretty HTML version of uni <-> seats
-OUTPUT_HTML = working_dir_path("list", args.slug, "html")
-
-# an html page with seat, student, and photo
-OUTPUT_CHART = working_dir_path("chart", args.slug, "html")
-
-# Now we're ready to assign seats
-assignments = {}
-
-with open(SEATS_IN_ORDER, "r") as f:
-    seats = f.readlines()
-seats = [s for s in seats if s[0] != '#']  # Strip comments
-seats = list(itertools.chain.from_iterable([z.strip().split() for z in seats]))
-
-assign_last = [x.strip() for x in open(ASSIGN_LAST).readlines()]
-assign_first = [x.strip() for x in open(ASSIGN_FIRST).readlines()]
-
-students = [tuple(s) for s in csv.reader(
-    open(STUDENT_LIST))][2:]  # Skip two header rows
-random.shuffle(students)
-
-# the assign_first/last students are shuffled randomly, so we need to pull them to the front/back
-reassign = [x for x in students if x[0] in assign_last]
-for x in reassign:
-    students.remove(x)
-students.extend(reassign)
-students.reverse()
-
-reassign = [x for x in students if x[0] in assign_first]
-for x in reassign:
-    students.remove(x)
-students.extend(reassign)
-
-# now loop over ordered seats assigning students
-for seat in seats:
-    if seat and seat not in assignments:
-        try:
-            assignments[seat] = students.pop()
-        except:
-            # Assigned all students
-            break
-        if args.debug:
-            print("assigned", assignments[seat], "to", seat)
-if students:
-    print("WARNING: unassigned students", students)
-
-if args.lefty:
-    with open(LSEATS_IN_ORDER, "r") as f:
-        lseats = f.readlines()
-    lseats = [s for s in lseats if s[0] != "#"]
-    lseats = list(itertools.chain.from_iterable(
-        [z.strip().split() for z in lseats]))
-
-    lstudents = [tuple(s) for s in csv.reader(open(LSTUDENT_LIST))][2:]
-    random.shuffle(lstudents)
-
-    for lseat in lseats:
-        if lseat and lseat not in assignments:
+    # now loop over ordered seats assigning students
+    for seat in seats:
+        if seat and seat not in assignments:
             try:
-                assignments[lseat] = lstudents.pop()
+                assignments[seat] = students.pop()
             except:
-                # All lefties assigned
+                # Assigned all students
                 break
-            if args.debug:
-                print("assigned lefty", assignments[lseat], "to", lseat)
-    if lstudents:
-        print("WARNING: unassigned lefties", lstudents)
+            if debug:
+                print("assigned", assignments[seat], "to", seat)
+    if students:
+        print("WARNING: unassigned students", students)
 
-# Write out the HTML roster
-with open(OUTPUT_HTML, "w") as html:
-    html.write(template.css.format(TITLE))
-    # sort by uni
-    for seat, student in sorted(assignments.items(), key=lambda x: x[1][2]):
-        html.write(
-            f"""<div><span class="uni">{student[2]}</span> <span class="seat">{seat}</span></div>""")
-    html.write("""</div></body>\n""")
+    if lefty:
+        with open(LSEATS_IN_ORDER, "r") as f:
+            lseats = f.readlines()
+        lseats = [s for s in lseats if s[0] != "#"]
+        lseats = list(itertools.chain.from_iterable(
+            [z.strip().split() for z in lseats]))
 
+        lstudents = [tuple(s) for s in csv.reader(open(LSTUDENT_LIST))][2:]
+        random.shuffle(lstudents)
 
-# dump to CSV as well
-output = csv.writer(open(OUTPUT_CSV, "w"))
-for seat, uni in assignments.items():
-    output.writerow(list(uni)[:2] + [seat])
+        for lseat in lseats:
+            if lseat and lseat not in assignments:
+                try:
+                    assignments[lseat] = lstudents.pop()
+                except:
+                    # All lefties assigned
+                    break
+                if debug:
+                    print("assigned lefty", assignments[lseat], "to", lseat)
+        if lstudents:
+            print("WARNING: unassigned lefties", lstudents)
 
+    # Write out the HTML roster
+    with open(OUTPUT_HTML, "w") as html:
+        html.write(template.css.format(TITLE))
+        # sort by uni
+        for seat, student in sorted(assignments.items(), key=lambda x: x[1][0]):
+            # for seat, student in assignments.items():
+            html.write(
+                f"""<div><span class="uni">{student[0]}</span> <span class="seat">{seat}</span></div>""")
+        html.write("""</div></body>\n""")
 
-# Write the chart
-room = [s for s in csv.reader(open(LAYOUT), delimiter="\t")]
-maxrow = max([len(x) for x in room])
+    # dump to CSV as well
+    output = csv.writer(open(OUTPUT_CSV, "w"))
+    for seat, uni in assignments.items():
+        output.writerow(list(uni)[:2] + [seat])
 
-with open(OUTPUT_CHART, "w") as seating_chart:
-    seating_chart.write(template.header)
-    for row in room:
-        seating_chart.write("<tr>")
-        row_count = 0
+    # Write the chart
+    room = [s for s in csv.reader(open(LAYOUT), delimiter="\t")]
+    maxrow = max([len(x) for x in room])
 
-        for seat in row:
-            row_count = row_count + 1
-            seating_chart.write(
-                "<td class='unselected' onclick='selectStudent(this)' >\n")
+    with open(OUTPUT_CHART, "w") as seating_chart:
+        seating_chart.write(template.header)
+        for row in room:
+            seating_chart.write("<tr>")
+            row_count = 0
 
-            try:
-                student = assignments[seat]
-                uni = student[2]
-                name = student[0]
-                full_name = student[1].split(", ")
-                full_name = f"{full_name[1]} {full_name[0]}"
-                # Used to check if file exists
-                # img_path = os.path.join(photos_path, uni + ".jpg")
-                img_path = os.path.join(photos_path, name + ".jpg")
-                # img_rel_path = os.path.join(
-                #     args.slug + "_files", uni + ".jpg")  # Inserted into HTML
-                img_rel_path = os.path.join(
-                    photos_path, name + ".jpg")  # Inserted into HTML
-                if os.path.isfile(img_path):
-                    seating_chart.write(
-                        f"""<span class="seat">{seat}</span><br> {uni}<br> <span class="name">{name}</span><br> <img src="{img_rel_path}">""")
-                else:
-                    print(f"WARNING: no img found for {img_path}")
-                    seating_chart.write(
-                        f"""<span class="seat">{seat}</span><br> {uni}<br> <span class="name">{name}</span><br> """)
-            except KeyError:
+            for seat in row:
+                row_count = row_count + 1
                 seating_chart.write(
-                    f"""<span class="seat">{seat}</span>""")
-            seating_chart.write("</td>\n")
+                    "<td class='unselected' onclick='selectStudent(this)' >\n")
 
-        for i in range(maxrow - row_count):
-            seating_chart.write("<td></td>\n")
+                try:
+                    student = assignments[seat]
+                    uni = student[0]
+                    name = student[1]
+                    full_name = student[1].split(", ")
+                    full_name = f"{full_name[1]} {full_name[0]}"
+                    # Used to check if file exists
+                    # img_path = os.path.join(photos_path, uni + ".jpg")
+                    img_path = os.path.join(photos_path, name + ".jpg")
+                    # img_rel_path = os.path.join(
+                    #     slug + "_files", uni + ".jpg")  # Inserted into HTML
+                    img_rel_path = os.path.join(
+                        photos_path, name + ".jpg")  # Inserted into HTML
+                    if os.path.isfile(img_path):
+                        seating_chart.write(
+                            f"""<span class="seat">{seat}</span><br> {uni}<br> <span class="name">{name}</span><br> <img src="{img_rel_path}">""")
+                    else:
+                        print(f"WARNING: no img found for {img_path}")
+                        seating_chart.write(
+                            f"""<span class="seat">{seat}</span><br> {uni}<br> <span class="name">{name}</span><br> """)
+                except KeyError:
+                    seating_chart.write(
+                        f"""<span class="seat">{seat}</span>""")
+                seating_chart.write("</td>\n")
 
-        seating_chart.write("</tr>\n\n")
-    seating_chart.write("</table></body>")
+            for i in range(maxrow - row_count):
+                seating_chart.write("<td></td>\n")
+
+            seating_chart.write("</tr>\n\n")
+        seating_chart.write("</table></body>")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Generate a seating chart based on the course roster")
+
+    parser.add_argument("slug",
+                        type=str,
+                        help="the \"out\" subdirectory to use as the working directory",
+                        metavar='<slug>')
+
+    parser.add_argument("layout",
+                        type=str,
+                        help="the seating chart layout (classroom name) to use",
+                        metavar="<layout>")
+
+    parser.add_argument("-t", "--title",
+                        default=None, type=str,
+                        help="human-readable name that will be written the top of seating chart",
+                        metavar="<title>")
+
+    parser.add_argument("-l", "--lefty",
+                        action="store_true",
+                        help="assign seats to lefty students")
+
+    parser.add_argument("-d", "--debug",
+                        action="store_true",
+                        help="print debug messages")
+
+    args = parser.parse_args()
+    arrange_seat(args.slug, args.layout, args.title, args.lefty, args.debug)
