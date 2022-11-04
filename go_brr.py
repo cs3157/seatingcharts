95c8315f2947745a235fe56c2ebcf2b49d3d8ad3
Leslie Chang <zz2474@columbia.edu>
2022-11-03 20:34:39 -0400

    clean go_brr.py

--- go_brr.py
+++ go_brr.py
@@ -5,7 +5,6 @@ import csv
 import glob
 import os
 import random
-import sys
 import math
 import seatingchart
 
@@ -50,30 +49,17 @@ def main(rooms_order: str, student_list: str, out_path: str = "out", html_path: 
         os.mkdir(path)
         os.system(("ln -s %s %s" %
                    ("images", path + "/" + "images")))
-        # print(("cp %s %s" %
-        #        ("images", path + "/" + "images")))
-        # os.system(("cp -rf %s %s" %
-        #            ("images", path + "/" + "images")))
-        csvfile = open(os.path.join(path, "roster_" + rname + ".csv"), "w")
-        output = csv.writer(csvfile)
-        # output.writerow("")
-        # output.writerow("")
-        for i in range(math.ceil(student_count / seat_count * int(room[1]))):
-            # for i in range(int(room[1])):
-            if(len(students) > 0):
-                student = random.choice(students)
-                output.writerow(student)
-                students.remove(student)
-        csvfile.flush()
+        with open(os.path.join(path, "roster_" + rname + ".csv"), "w") as csvfile:
+            output = csv.writer(csvfile)
+            for i in range(math.ceil(student_count / seat_count * int(room[1]))):
+                if(len(students) > 0):
+                    student = random.choice(students)
+                    output.writerow(student)
+                    students.remove(student)
+            csvfile.flush()
         seatingchart.arrange_seat(rname, rname)
-        # os.system("./seatingchart.py %s %s" %
-        #           (rname, rname))
         os.system("cp %s/%s/chart_%s.html %s/%s.html" %
                   (out_path, rname, rname, html_path, rname))
-        os.system(
-            f"perl -F, -lane 's/$F[2]/$F[2], {rname}/; print' {out_path}/{rname}/list_{rname}.csv >> {html_path}/seat.csv")
-    #    os.system("cp %s/%s/list_%s.csv %s/%s.csv" %
-    #              (out_path, rname, rname, html_path, rname))
         os.system(f"chmod 644 {html_path}/{rname}.html")
     os.system(f"cp -r images {html_path}/images")
     os.system(f"chmod 711 {html_path}/images")
@@ -88,17 +74,16 @@ if __name__ == "__main__":
 
     parser.add_argument("rooms",
                         type=str,
-                        metavar='<rooms>')
-
+                        metavar='<rooms>', help='Room seats allocation')
     parser.add_argument("roster",
                         type=str,
-                        metavar='<roster>')
+                        metavar='<roster>', help='Student roster from courseworks')
     parser.add_argument("--out", "--O",
                         type=str, default="out",
-                        metavar='Output Path')
+                        metavar='', help='Output Path')
     parser.add_argument("--html", "--H",
                         type=str, default="~/html/seating",
-                        metavar='HTML Path')
+                        metavar='', help='HTML Path')
     args = parser.parse_args()
     rooms_order = args.rooms
     student_list = args.roster
