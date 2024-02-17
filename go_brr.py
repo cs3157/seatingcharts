db739a9947d80b3fa51383b47642feba0bd8c509
Tal Zussman <tz2294@columbia.edu>
2024-02-16 21:51:12 -0500

    Get working and clean up a little

--- go_brr.py
+++ go_brr.py
@@ -2,17 +2,18 @@
 
 import argparse
 import csv
-import glob
+import math
 import os
 import random
 import shutil
-import sys
-import math
 from pathlib import Path
-from csv2pdf import convert
+
+import csv2pdf
 
 OUT_PATH = Path.cwd() / "out"
 HTML_PATH = Path.home() / "html" / "seating"
+HTML_IMAGES_PATH = HTML_PATH / "images"
+HTML_PDF_PATH = HTML_PATH / "seat.pdf"
 
 def rename_images(img_path: Path) -> None:
     for img in img_path.glob("*"):
@@ -31,7 +32,7 @@ def create_html_directory(html_path: Path) -> None:
 
 
 def init_seat_csv(seat_csv_path: Path) -> None:
-    seat_csv_path.write_text("Uni, Name, Room, Seat\n")
+    seat_csv_path.write_text("Uni,Name,Seat,Room\n")
     seat_csv_path.chmod(0o744)
 
 
@@ -41,10 +42,10 @@ def main():
     parser.add_argument("roster", type=str, metavar='<roster>')
     args = parser.parse_args()
 
-    ROOMS_IN_ORDER = Path(args.rooms)
-    STUDENT_LIST = Path(args.roster)
+    ROOM_FILE = Path(args.rooms)
+    STUDENT_FILE = Path(args.roster)
 
-    with STUDENT_LIST.open() as f:
+    with STUDENT_FILE.open() as f:
         students = [tuple(s) for s in csv.reader(f)]
 
     random.shuffle(students)
@@ -59,49 +60,62 @@ def main():
     seat_csv_path = HTML_PATH / "seat.csv"
     init_seat_csv(seat_csv_path)
 
-    with ROOMS_IN_ORDER.open('r') as f:
+    with ROOM_FILE.open('r') as f:
         rooms = [r.strip().split() for r in f.readlines() if r.strip()]
 
-    print(f"rooms:{rooms}")
+    print(f"Rooms: {rooms}")
     for _, count in rooms:
         seat_count += int(count)
-    print(f"Total seats:{seat_count}")
+    print(f"Total seats: {seat_count}")
+
+    student_index = 0
 
     for room in rooms:
-        rname = room[0]
-        path = Path(OUT_PATH) / rname
-        shutil.rmtree(path, ignore_errors=True)
-        path.mkdir(parents=True)
-        (path / "images").symlink_to(Path("images"))
+        rname, rcount = room[0], room[1]
+        ROOM_OUT_PATH = Path(OUT_PATH) / rname
+        ROOM_ROSTER_PATH = ROOM_OUT_PATH / f"roster_{rname}.csv"
+        ROOM_LIST_PATH = ROOM_OUT_PATH / f"list_{rname}.csv"
+        ROOM_CHART_PATH = ROOM_OUT_PATH / f"chart_{rname}.html"
+        ROOM_IMAGES_PATH = ROOM_OUT_PATH / "images"
 
-        with (path / f"roster_{rname}.csv").open("w", newline='') as csvfile:
+        HTML_ROOM_PATH = HTML_PATH / f"{rname}.html"
+
+        shutil.rmtree(ROOM_OUT_PATH, ignore_errors=True)
+        ROOM_OUT_PATH.mkdir(parents=True)
+        ROOM_IMAGES_PATH.symlink_to(Path("images"))
+
+        with ROOM_ROSTER_PATH.open("w", newline='') as csvfile:
             output = csv.writer(csvfile)
-            output.writerow([""] * 2)
-            for _ in range(math.ceil(student_count / seat_count * int(room[1]))):
-                if students:
-                    student = random.choice(students)
+            num_seats = math.ceil(student_count / seat_count * int(rcount))
+            for _ in range(num_seats):
+                if student_index < student_count:
+                    student = students[student_index]
+                    student_index += 1
                     output.writerow(student)
-                    students.remove(student)
             csvfile.flush()
 
-        os.system(f"./seatingchart.py {rname} {rname}")
-        shutil.copy(path / f"chart_{rname}.html", HTML_PATH / f"{rname}.html")
+        # Check if the os.system call is successful
+        if os.system(f"./seatingchart.py {rname} {rname}") != 0:
+            print("\033[01;91mError: seatingchart.py failed\033[0m")
+            return
 
-        with (path / f"list_{rname}.csv").open('r') as list_file:
+        shutil.copy(ROOM_CHART_PATH, HTML_ROOM_PATH)
+
+        with ROOM_LIST_PATH.open('r') as list_file:
             with seat_csv_path.open('a') as seat_file:
                 for line in list_file:
-                    modified_line = line.replace(f"{room[0]},", f"{room[0]}, {rname},")
+                    modified_line = line.strip() + f",{rname}\n"
                     seat_file.write(modified_line)
 
-        (HTML_PATH / f"{rname}.html").chmod(0o644)
+        HTML_ROOM_PATH.chmod(0o644)
 
-    shutil.copytree("images", HTML_PATH / "images")
-    (HTML_PATH / "images").chmod(0o711)
-    for img in (HTML_PATH / "images").glob("*.*"):
+    shutil.copytree("images", HTML_IMAGES_PATH)
+    HTML_IMAGES_PATH.chmod(0o711)
+    for img in HTML_IMAGES_PATH.glob("*.*"):
         img.chmod(0o644)
 
-    convert(seat_csv_path, HTML_PATH / "seat.pdf")
-    (HTML_PATH / "seat.pdf").chmod(0o644)
+    csv2pdf.convert(seat_csv_path, HTML_PDF_PATH)
+    HTML_PDF_PATH.chmod(0o644)
     seat_csv_path.unlink()
 
     print("\033[01;92mSuccess!\033[0m")
